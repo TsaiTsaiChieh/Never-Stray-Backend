9201f1c184ac4023a6c59c525d7103701e0aa563
d0177381@gmail.com <>
2021-08-07 20:46:09 +0800

    :label: add type annotation

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
 import dotenv from 'dotenv'
+import express, {Application, Request, Response} from 'express'
+
 dotenv.config()
-import express from 'express'
+const app: Application = express()
 
-const app = express()
+const APP_PORT: Number = parseInt(<string>process.env.APP_PORT, 10)
 
-const {APP_PORT} = process.env
-
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
 
 // 印出 env 的檔案 for bug
-app.get('/env', (req, res) => {
+app.get('/env', (req: Request, res: Response) => {
   res.json(process.env)
 })
 
-app.listen(APP_PORT, () => {
+app.listen(APP_PORT, (): void => {
   console.log(`App listening at http://localhost:${APP_PORT}`)
 })
