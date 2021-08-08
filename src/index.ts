0d98c91427e5a57131487fe1eae82cc3e6d0de85
d0177381@gmail.com <>
2021-08-08 22:19:50 +0800

    :sparkles: add connection endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import dotenv from 'dotenv'
-import express, {Application, Request, Response} from 'express'
+import express, {Application} from 'express'
+
+import * as connectionRouter from './routes/connection'
 
 dotenv.config()
 const app: Application = express()
-
 const APP_PORT: Number = parseInt(<string>process.env.APP_PORT, 10)
+
+app.use('/connection', connectionRouter.default)
 
 app.listen(APP_PORT, (): void => {
   console.log(`App listening at http://localhost:${APP_PORT}`)
