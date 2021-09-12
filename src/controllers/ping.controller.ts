5b2724f6368654fbf862025abcfb6c3f760add3f
Tsai-Chieh <d0177381@gmail.com>
2021-09-12 22:15:56 +0800

    :sparkles: add ping controller

---
+++ src/controllers/ping.controller.ts
@@ -0,0 +1,30 @@
+/* eslint-disable require-jsdoc */
+import {Request, Response} from 'express'
+import {Controller, Get, Req, Res} from 'routing-controllers'
+import {getConnectionManager} from 'typeorm'
+
+@Controller('/ping')
+export class ConnectionController {
+  @Get('/api')
+  pingAPI(@Req() req: Request, @Res() res: Response) {
+    return res.send('Pong')
+  }
+
+  @Get('/db')
+  async pingDB(@Req() req: Request, @Res() res: Response) {
+    const con = getConnectionManager().get()
+    try {
+      let canConnect: boolean = false
+      // Try to connect
+      if (!con.isConnected) await con.connect()
+      // Store connection result
+      canConnect = con.isConnected
+      if (!canConnect) return res.send('Database connection failed')
+      else return res.send('Database connection successfully')
+    } catch (error) {
+      return res.send(`Database error happened: ${error}`)
+    } finally {
+      await con.close()
+    }
+  }
+}
