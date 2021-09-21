9d66a7f5122323ec8c7e8fea9ceb8960825aa9e2
Tsai-Chieh <d0177381@gmail.com>
2021-09-21 22:18:53 +0800

    :sparkles: add init area data script

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,6 @@ echo "Wait for all services to start"
 sleep 10;
 echo "Run migration ..."
 yarn typeorm migration:run
+echo "Initializing area data ..."
+yarn ts-node ./src/scripts/init-area.ts
 echo "=== END ==="
