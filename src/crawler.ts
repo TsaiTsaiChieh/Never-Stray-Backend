bd4c0a629fd6f9c21f60ba9deed8279812406ddf
Tsai-Chieh <d0177381@gmail.com>
2021-10-17 22:37:50 +0800

    :construction: work in progress

--- src/crawler.ts
+++ src/crawler.ts
@@ -2,18 +2,18 @@ import dotenv from 'dotenv'
 import {scheduleJob} from 'node-schedule'
 
 import {initializerApp} from './initializer'
-import getShelterData from './jobs/shelter-data'
+import {getShelterData} from './jobs/shelter-data'
 
 dotenv.config()
 
 const JOB_PORT: number = parseInt(process.env.JOB_PORT!);
 (async () => {
-    const {app, db} = await initializerApp()
-    app.listen(JOB_PORT, (): void => {
-      console.log(`Crawler listening at http://localhost:${JOB_PORT}`)
-      scheduleJob('* * * * * *', function() {
-        getShelterData(db)
-        })
+  const {app, db} = await initializerApp()
+  app.listen(JOB_PORT, (): void => {
+    console.log(`Crawler listening at http://localhost:${JOB_PORT}`)
+    scheduleJob('* * * * * *', function() {
+      getShelterData(db)
     })
-  })()
+  })
+})()
 
--- src/jobs/shelter-data.ts
+++ src/jobs/shelter-data.ts
@@ -1,29 +1,68 @@
+/* eslint-disable camelcase */
 /* eslint-disable require-jsdoc */
-import axios from 'axios'
+import dotenv from 'dotenv'
+import axios, {AxiosResponse} from 'axios'
 import {Connection} from 'typeorm'
-
+import _ from 'lodash'
 import {Area} from '../entity/Area'
-
-// dotenv.config()
-
-// class Shelter {
-//     constructor() {
-
-//     }
-//     public async getData() {
-//         const db = (await initializerApp()).db
-//         const findArea = await db.getRepository(Area).findOne()
-//         console.log(findArea)
-
-//         console.log(process.env.NATIONAL_ANIMAL_SHELTER)
-//     }
-// }
-async function getShelterData(db: Connection) {
-  // const findArea = await db.getRepository(Area).findOne()
-  // console.log(findArea)
-  const url: string = process.env.NATIONAL_ANIMAL_SHELTER!
-  // const data = await axios.get(`${url}`)
-  console.log(url)
+dotenv.config()
+type ShelterData = {
+  animal_id: number
+  animal_subid: string
 }
 
-export default getShelterData
+export class Shelter {
+  public url: string = process.env.NATIONAL_ANIMAL_SHELTER!
+  // private db: Connection
+  private batch: number = 100
+
+  constructor() {
+
+  }
+  // constructor(db: Connection) {
+  //   this.db = db
+  // }
+  // get data
+
+  public async getData() {
+    const allData: any[] = []
+    for (let page = 0; ; page++) {
+      const response: AxiosResponse<ShelterData[]> = await axios.get(
+        `${this.url}
+        &$top=${this.batch}
+        &$skip=${this.batch * page}
+        &animal_status=OPEN`,
+      )
+
+      const data: ShelterData[] = response.data
+      if (!data.length) break
+      _.forEach(data, (val) => allData.push(val),
+      )
+    }
+    return allData
+  }
+  // save to db
+  // const findArea = await this.db.getRepository(Area).findOne()
+  // console.log(findArea)
+}
+// async function getShelterData(db: Connection) {
+//   // const findArea = await db.getRepository(Area).findOne()
+//   // console.log(findArea)
+//   const url: string = process.env.NATIONAL_ANIMAL_SHELTER!
+//   // const data = await axios.get(`${url}`)
+//   console.log(url)
+// }
+
+// export default getShelterData
+// export async function getShelterData(db: Connection) {
+//   const shelter = new Shelter(db = db)
+//   await shelter.getData()
+// }
+
+export async function getShelterData() {
+  const shelter = new Shelter()
+  const data = await shelter.getData()
+  console.log(data.length)
+}
+
+getShelterData()
