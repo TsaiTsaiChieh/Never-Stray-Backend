df62affeb7cbd691a9ac38d6cd72d7c7cbfeab28
Tsai-Chieh <d0177381@gmail.com>
2021-11-03 13:20:51 +0800

    :sparkles: add findOne & format

--- src/utils/basic-repository.ts
+++ src/utils/basic-repository.ts
@@ -1,4 +1,10 @@
-import {getRepository, ObjectType, Repository} from 'typeorm'
+import {
+  FindConditions,
+  FindOneOptions,
+  getRepository,
+  ObjectType,
+  Repository,
+} from 'typeorm'
 
 /** Class representing a basic repository  */
 export class BasicRepository<T> {
@@ -11,20 +17,37 @@ export class BasicRepository<T> {
     this.entity = entity
     this.repository = getRepository(entity)
   }
+
   /**
    * 儲存單一 Entity<T> 的資料
+   *
    * @param  {T} data - Single data to save
    * @return {Promise<T>}
    */
-  save(data: T):Promise<T> {
+   save(data: T):Promise<T> {
     return this.repository.save(data)
   }
+
   /**
    * 儲存多筆 Entity<T> 的資料
+   *
    * @param  {T} data - Multiple data to save
    * @return {Promise<T>}
    */
   saveMany(data: T[]):Promise<T[]> {
     return this.repository.save(data)
+  }
+
+  /**
+   * 尋找符合條件的第一筆資料
+   *
+   * @param  {FindConditions<T>} [conditions] - Used for find operations
+   * @param  {FindOneOptions<T>} [options] - Special criteria, like: select,
+   * where, relation, join, order
+   * @return {Promise<T|undefined>}
+   */
+  findOne(conditions?: FindConditions<T>,
+    options?: FindOneOptions<T>) : Promise<T|undefined> {
+    return this.repository.findOne(conditions, options)
   }
 }
