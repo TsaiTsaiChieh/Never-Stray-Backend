e74b333286126377b27a3d2174f3cb729d045589
Tsai-Chieh <d0177381@gmail.com>
2021-11-29 00:12:13 +0800

    :tada: add pet typings

---
+++ src/typings/pet.d.ts
@@ -0,0 +1,18 @@
+/**
+ * 資料來源
+ *
+ * @enum {string} gov 政府
+ * @enum {string} map 認養地圖
+ * @enum {string} own 使用者上傳
+ */
+type PetRefType = 'gov' | 'map' | 'own'
+
+/**
+ * 寵物性別
+ *
+ * @enum {string} F female
+ * @enum {string} M male
+ * @enum {string} U unknown
+ */
+type PetSexType = 'F' | 'M' | 'U'
+
