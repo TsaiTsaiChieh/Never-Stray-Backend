61dd6095e87d460ce9f38d799363397f07746712
Tsai-Chieh <d0177381@gmail.com>
2021-11-29 11:33:38 +0800

    :tada: init area.d.ts

---
+++ src/typings/area.d.ts
@@ -0,0 +1,11 @@
+/**
+ * 區域分布
+ *
+ * @enum {string} E 東部
+ * @enum {string} W 西部
+ * @enum {string} S 南部
+ * @enum {string} N 北部
+ * @enum {string} M 中部
+ */
+type AreaRegionType = 'E' | 'W' | 'S' | 'N' | 'M'
+
