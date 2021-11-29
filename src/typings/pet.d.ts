4c2b7e69a32b9f3ad366ea6150c9f68d5c46438d
Tsai-Chieh <d0177381@gmail.com>
2021-11-29 10:32:04 +0800

    :sparkles: add sex, status, kind types

--- src/typings/pet.d.ts
+++ src/typings/pet.d.ts
@@ -16,3 +16,31 @@ type PetRefType = 'gov' | 'map' | 'own'
  */
 type PetSexType = 'F' | 'M' | 'U'
 
+/**
+ * 寵物年齡
+ *
+ * @enum {string} F female
+ * @enum {string} M male
+ * @enum {string} U unknown
+ */
+ type PetAgeType = 'A' | 'C' | 'U'
+
+/**
+ * 寵物狀態
+ *
+ * @enum {string} Unknown 未知
+ * @enum {string} Open 開放認養
+ * @enum {string} Adopted 已被認養
+ * @enum {string} Other 其他
+ * @enum {string} Dead 死亡
+ */
+ type PetStatusType = 'Unknown' | 'Open' | 'Adopted' | 'Other' | 'Dead'
+
+ /**
+ * 寵物狀態
+ *
+ * @enum {string} D 狗
+ * @enum {string} C 貓
+ * @enum {string} O 其他
+ */
+  type PetKindType = 'D' | 'C' | 'O'
