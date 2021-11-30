9c9babed71f98ccaa952e47b9babe0bdb6b9f87a
Tsai-Chieh <d0177381@gmail.com>
2021-11-30 10:23:13 +0800

    :label: add SQLErrorType, ErrorType & format

--- src/typings/types.d.ts
+++ src/typings/types.d.ts
@@ -9,8 +9,46 @@ type triStateType = 1 | 0 | -1
 
 /**
  * 排序方式
- * 
+ *
  * @enum {string} ASC 升幂
  * @enum {string} DESC 降冪
  */
 type OrderType = 'ASC' | 'DESC'
+
+/**
+ * SQL 錯誤訊息的架構
+ *
+ * @type {string} query - parameterized query
+ * @type {string[]} parameters
+ * @type {string} code
+ * @type {number} errno
+ * @type {string} sqlState
+ * @type {string} sqlMessage - SQL error message
+ * @type {string} sql - SQL raw query
+ */
+type SQLErrorType = {
+  query: string
+  parameters: string[]
+  code: string,
+  errno: number,
+  sqlState: string,
+  sqlMessage: string,
+  sql: string,
+}
+
+/**
+ * 錯誤訊息的架構
+ *
+ * @type {string} message
+ * @type {string} [details]
+ * @type {number} code - http status code
+ * @enum {string} status - fail or error
+ * @type {boolean} isOperational
+ */
+ type ErrorType = {
+  message: string
+  details?: string
+  code: number,
+  status: 'fail' | 'error',
+  isOperational: boolean,
+}
