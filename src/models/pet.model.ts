befc51e2e06f3db500b6bcea183d12f27687aab2
Tsai-Chieh <d0177381@gmail.com>
2021-12-16 22:17:04 +0800

    :recycle: return DBError

--- src/models/pet.model.ts
+++ src/models/pet.model.ts
@@ -1,23 +1,20 @@
 /* eslint-disable require-jsdoc */
-import httpStatus from 'http-status'
 import safeAwait from 'safe-await'
 
 import {Pet} from '../entity/pet.entity'
 import {PetRepository} from '../repositories/pet.repository'
-import {AppError} from '../utils/app-error'
+import {DBError} from '../utils/app-error'
 
 export class PetModel {
   private petRepository: PetRepository
-  // public appError
   constructor() {
     this.petRepository = new PetRepository()
-    // this.appError = new AppError()
   }
 
   async getAll(query: PetQuery):Promise<any> {
-    const [error, result]: [any, Pet[]] =
+    const [error, result]: [SQLErrorType, Pet[]] =
     await safeAwait(this.petRepository.findByFilters(query))
-    if (error) return new AppError(error, httpStatus.INTERNAL_SERVER_ERROR)
+    if (error) return new DBError(error.sqlMessage, error.sql)
     return result
   }
 }
