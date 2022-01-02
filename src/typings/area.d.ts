c3b171e051c0bae28617d8891a79648485638615
Tsai-Chieh <d0177381@gmail.com>
2022-01-02 21:53:43 +0800

    :label: add AreaCityType

--- src/typings/area.d.ts
+++ src/typings/area.d.ts
@@ -8,3 +8,53 @@
  * @enum {string} M 中部
  */
 type AreaRegionType = 'E' | 'W' | 'S' | 'N' | 'M'
+
+/**
+ * 縣市
+ *
+ * @enum {string} 2 臺北市
+ * @enum {string} 3 新北市
+ * @enum {string} 4 基隆市
+ * @enum {string} 5 宜蘭縣
+ * @enum {string} 6 桃園市
+ * @enum {string} 7 新竹縣
+ * @enum {string} 8 新竹市
+ * @enum {string} 9 苗栗縣
+ * @enum {string} 10 臺中市
+ * @enum {string} 11 彰化縣
+ * @enum {string} 12 南投縣
+ * @enum {string} 13 雲林縣
+ * @enum {string} 14 嘉義縣
+ * @enum {string} 15 嘉義市
+ * @enum {string} 16 臺南市
+ * @enum {string} 17 高雄市
+ * @enum {string} 18 屏東縣
+ * @enum {string} 19 花蓮縣
+ * @enum {string} 20 臺東縣
+ * @enum {string} 21 澎湖縣
+ * @enum {string} 22 金門縣
+ * @enum {string} 23 連江縣
+ */
+type AreaCityType =
+  | '2'
+  | '3'
+  | '4'
+  | '5'
+  | '6'
+  | '7'
+  | '8'
+  | '9'
+  | '10'
+  | '11'
+  | '12'
+  | '13'
+  | '14'
+  | '15'
+  | '16'
+  | '17'
+  | '18'
+  | '19'
+  | '20'
+  | '21'
+  | '22'
+  | '23'
