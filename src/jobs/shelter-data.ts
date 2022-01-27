c9a8080153d6036afe890c2497ece6df9abe335c
Tsai-Chieh <d0177381@gmail.com>
2022-01-27 20:30:00 +0800

    :truck: move ShelterData to declaration file

--- src/jobs/shelter-data.ts
+++ src/jobs/shelter-data.ts
@@ -17,36 +17,6 @@ import {
   ternaryConvert,
 } from '../utils/value-convert'
 
-type ShelterData = {
-  animal_id: number
-  animal_subid: string
-  animal_area_pkid: number
-  animal_shelter_pkid: number
-  animal_place: string
-  animal_kind: string
-  animal_sex: string
-  animal_bodytype: string
-  animal_colour: string
-  animal_age: string
-  animal_sterilization: string
-  animal_bacterin: string
-  animal_foundplace: string
-  animal_title: string
-  animal_status: string
-  animal_remark: string
-  animal_caption: string
-  animal_opendate: string
-  animal_closeddate: string
-  animal_update: string
-  animal_createtime: string
-  shelter_name: string
-  album_file: string
-  album_update: string
-  cDate: string
-  shelter_address: string
-  shelter_tel: string
-}
-
 /** Class representing a pet repository  */
 export class Shelter {
   public url: string = process.env.NATIONAL_ANIMAL_SHELTER!
