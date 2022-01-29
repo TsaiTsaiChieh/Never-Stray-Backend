6f7d33f14f196a4466b3f6011d07474f5409b7f5
Tsai-Chieh <d0177381@gmail.com>
2022-01-29 18:49:17 +0800

    :bug: fix when yarn start error: Cannot use import statement outside a module

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,5 +1,8 @@
 import {Connection, createConnection} from 'typeorm'
 
+import {Area} from '../entity/area.entity'
+import {Pet} from '../entity/pet.entity'
+
 export const connection = async (): Promise<Connection> => {
   return createConnection({
     type: 'mysql',
@@ -8,7 +11,7 @@ export const connection = async (): Promise<Connection> => {
     username: process.env.TYPEORM_USERNAME,
     password: process.env.TYPEORM_PASSWORD,
     database: process.env.TYPEORM_DATABASE,
-    entities: ['**/src/entity/*{.ts,.js}'],
+    entities: [Area, Pet],
     logging: false,
   })
 }
